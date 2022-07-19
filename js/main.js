3e03c3caa23c854fcfc00535aea4822ed9272c00
Renan Cesar <cesar.renan@hotmail.com>
2022-07-19 20:10:57 -0300

    Finalizando gráfico e arrumando css

--- js/main.js
+++ js/main.js
@@ -6,6 +6,33 @@ const navesContador = document.getElementById("naves");
 preencherContadores();
 preencherTabela();
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(desenharGrafico);
+
+async function desenharGrafico() {
+  const response = await swapiGet('vehicles/')
+  const vehiclesArray = response.data.results;
+
+  const dataArray = [];
+  dataArray.push(["Veículos", "Passageiros"]);
+  vehiclesArray.forEach(vehicle => {
+    dataArray.push([vehicle.name, Number(vehicle.passengers)]);
+  });
+
+  var data = google.visualization.arrayToDataTable(dataArray);
+
+  var options = {
+    title: 'Maiores Veículos',
+    legend: 'none'
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
+
+
+
 function preencherContadores(){
    Promise.all([
      swapiGet("people/"),
